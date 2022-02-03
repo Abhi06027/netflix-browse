915edde5613eaa8525cc6afb1fc309d36a5e4be0
Abhi06027 <abhinavk67@gmail.com>
2022-02-03 17:37:50 +0530

    netflix

--- src/Components/Phone.js
+++ src/Components/Phone.js
@@ -6,30 +6,31 @@ export default function Phone() {
              Top 10 in India Today  
              
                  </h1>
+                 {/* sm:ml-16 md:ml-16 lg:ml-24 */}
+             <div className=' flex px-5 sm:px-5 md:px-8 lg:px-16 space-x-2  sm:space-x-2 md:space-x-12 lg:space-x-20 object-contain  py-1 sm:py-2 md:py-2 lg:py-3  overflow-scroll scrollbar-hide cursor-pointer     ' > 
                  
-             <div className=' flex  ml-16 sm:ml-16 md:ml-16 lg:ml-24    space-x-20 object-contain   sm:py-2 md:py-2 lg:py-3  overflow-scroll scrollbar-hide cursor-pointer     ' > 
-                 
-                 <img className=" text-white  w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/13.jpg" alt="" />
+                 <img className=" text-white  w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/13.jpg" alt="" />
                
-                   <img className=" text-white  w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/14.png" alt="" />
+                   <img className=" text-white  w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/14.png" alt="" />
                 
                
-                <img className=" text-white      w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out " src="./images/15.png" alt="" />
+                <img className=" text-white      w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px] object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out " src="./images/15.png" alt="" />
               
                  
                   
-                 <img className=" text-white    w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]  object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/16.png" alt="" />
+                 <img className=" text-white     w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px]  object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/16.png" alt="" />
                  
                   
                   
                  
            
-               <img className=" text-white    w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]  object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/17.png" alt="" />
-               <img className=" text-white  w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]   object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/18.png" alt="" />
-               <img className=" text-white  w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]   object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/19.png" alt="" />
+               <img className=" text-white     w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px]  object-contain   rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/17.png" alt="" />
+               <img className=" text-white   w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px]   object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/18.png" alt="" />
+               <img className=" text-white   w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px]   object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/19.png" alt="" />
             
-                <img className=" text-white w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]  object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/20.png" alt="" />
-     <img className=" text-white  w-[120px] h-[120px]  sm:[120px] sm:h-[120px] md:w-[150px] md:h-[200px] lg:w-[150px] lg:h-[200px]  object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/21.png" alt="" />
+                <img className=" text-white  w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px]  object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/20.png" alt="" />
+      <img className=" text-white  w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px] object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/21.png" alt="" />
+      <img className=" text-white  w-[150px] h-[150px]  sm:[150px] sm:h-[150px] md:w-[200px] md:h-[200px] lg:w-[200px] lg:h-[200px] object-contain  rounded-sm transform lg:hover:scale-105 transition-transform duration-500 ease-in-out  " src="./images/21.png" alt="" />
     </div>
 
 
