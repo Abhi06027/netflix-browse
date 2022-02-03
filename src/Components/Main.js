fbb933ccc0adb833c746bbc4565d3ac1a82fa378
Abhi06027 <abhinavk67@gmail.com>
2022-02-04 03:04:56 +0530

    netflix

--- src/Components/Main.js
+++ src/Components/Main.js
@@ -6,7 +6,7 @@ export default function Main() {
       <main className=" ">
         <div>
           <img
-            className="  lg:mt-32   w-[100px] sm:w-[200px] md:w-[300px] lg:w-[500px] ml-5  ml:px-5 md:ml-8 lg:ml-20 py-2 sm:py-2 md:py-3  lg:py-5   "
+            className="    lg:mt-32   w-[100px] sm:w-[200px] md:w-[300px] lg:w-[500px] ml-5  sm:ml-5 md:ml-8 lg:ml-20 py-2 sm:py-2 md:py-3  lg:py-5   "
             src="./images/ameri.png"
             alt=""
           />
@@ -16,8 +16,7 @@ export default function Main() {
 <p className='  text-white  pl-4 text-3xl font-bold  ' > #1 in India Today </p> 
 </div> */}
         <div>
-          <p className=" cursor-pointer text-white  pl-5 px-16 sm:px-16 md:px-8 lg:px-20 text-[10px] sm:text-xs md:text-sm lg:text-2xl font-medium max-w-xs sm:max-w-xs md:max-w-sm lg:max-w-2xl ">
-            {" "}
+          <p className=" cursor-pointer text-white  pl-5 px-8  md:px-8 lg:px-20 text-[10px] sm:text-xs md:text-sm lg:text-2xl font-medium max-w-xs sm:max-w-xs md:max-w-sm lg:max-w-2xl ">
             Erik, the prude virgin cousin of Matt and Steve Stifler, comes out
             of his shell when he and his friends participate in a naked run on
             campus.
@@ -32,7 +31,7 @@ export default function Main() {
             />{" "}
             <span> Play </span>
           </button>
-          <button className="  flex justify-center space-x-3 items-center bg-gray-200 bg-opacity-20 hover:bg-opacity-30 h-7 w-32 text-sm  rounded-md  font-bold lg:h-14 lg:w-48 lg:text-xl text-white">
+          <button className="  flex justify-center space-x-1 items-center bg-gray-200 bg-opacity-20 hover:bg-opacity-30 h-7 w-28 text-sm  rounded-md  font-bold lg:h-14 lg:w-48 lg:text-xl text-white">
             <img className="w-5 lg:w-9   " src="./images/info.png" alt="" />{" "}
             <span className=""> More Info </span>
           </button>
